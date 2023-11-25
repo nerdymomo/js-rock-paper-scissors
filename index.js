07641953baf812eba9430a09d07a41882e1b3a2a
Mohamed Hamza <36870503+nerdymomo@users.noreply.github.com>
2023-11-26 00:03:58 +0200

    Wrote pseudo-code to help me write the program

---
+++ index.js
@@ -0,0 +1,5 @@
+// Make and array that contains rock-paper-scissors
+// Make a function that randomly returns one of those words
+// Make a function to get user's input and it has to be case-insensitive
+// Include an error message if the input doesn't match rock-paper-scissors
+// Match the user's input with the computer's choice and return a string for the winning or losing
