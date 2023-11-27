d0706bbcff93c5e956dab22f5b37f2e6222c0478
Mohamed Hamza <36870503+nerdymomo@users.noreply.github.com>
2023-11-27 16:27:42 +0200

    wrote the the game logic

--- index.js
+++ index.js
@@ -1,5 +1,52 @@
-// Make and array that contains rock-paper-scissors
-// Make a function that randomly returns one of those words
-// Make a function to get user's input and it has to be case-insensitive
-// Include an error message if the input doesn't match rock-paper-scissors
-// Match the user's input with the computer's choice and return a string for the winning or losing
+let options = ["rock", "paper", "scissors"];
+
+function computerChoice() {
+    let randomIndex = Math.round(Math.random() * options.length)
+    let randomChoice = options[randomIndex];
+    return randomChoice
+}
+
+const rules = choice => {
+    const lowerCaseChoice = choice.toLowerCase();
+    let defeat
+    switch (lowerCaseChoice) {
+        case "rock":
+            defeat = "paper";
+            break;
+        case "paper":
+            defeat = "scissors";
+            break;
+        case "scissors":
+            defeat = "rock";
+            break;
+        default:
+            defeat = "tie";
+    }
+    return defeat;
+};
+
+function playRound() {
+    let playerChoice = prompt("What's your move? Rock, Paper or scissors?").toLowerCase()
+    const playerSelection = playerChoice;
+    const computerSelection = computerChoice();
+    const beatsPlayerSelection = rules(playerSelection);
+    const beatsComputerSelection = rules(computerSelection)
+
+    if (playerSelection === beatsComputerSelection) {
+        console.log(`${playerSelection} beats ${computerSelection}, you wins!`);
+    } else if (computerSelection === beatsPlayerSelection) {
+        console.log(`${computerSelection} beats ${playerSelection}, computer wins!`)
+    } else {
+        console.log("it's a tie!")
+    }
+}
+
+function game() {
+    let i = 0;
+    while (i < 5) {
+        playRound();
+        i++;
+    }
+}
+
+console.log(game())
